e43e84954beb4346eb08c2634926ca0b02923662
hesham <17p6020@eng.asu.edu.eg>
2020-10-30 18:27:02 +0200

    done

--- lib/Screens/Onboarding.dart
+++ lib/Screens/Onboarding.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:liquid_swipe/liquid_swipe.dart';
 
 class OnBoarding extends StatefulWidget {
   @override
@@ -8,6 +9,175 @@ class OnBoarding extends StatefulWidget {
 class _OnBoardingState extends State<OnBoarding> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final pages = [
+      Container(
+        color: Color(0xff3937bf),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Image.asset('assets/marketing1.JPG'),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Best Digital Solution',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 25,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  ButtonTheme(
+                    height: 50,
+                    minWidth: 150,
+                    child: OutlineButton(
+                      borderSide: BorderSide(color: Colors.white),
+                      textColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Text(
+                        'Skip',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      Container(
+        color: Color(0xff27b56f),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Image.asset('assets/marketing2.png'),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Achive Your Goal',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 25,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  ButtonTheme(
+                    height: 50,
+                    minWidth: 150,
+                    child: OutlineButton(
+                      borderSide: BorderSide(color: Colors.white),
+                      textColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Text(
+                        'Skip',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      Container(
+        color: Color(0xfff46d37),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Image.asset('assets/marketing3.png'),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Increase Your Value',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 25,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  ButtonTheme(
+                    height: 50,
+                    minWidth: 150,
+                    child: OutlineButton(
+                      borderSide: BorderSide(color: Colors.white),
+                      textColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Text(
+                        'Get Started',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    ];
+    return Scaffold(
+      body: LiquidSwipe(
+        pages: pages,
+        enableSlideIcon: true,
+        positionSlideIcon: 0,
+        slideIconWidget: Icon(
+          Icons.arrow_back_ios,
+          size: 30,
+          color: Colors.white,
+        ),
+      ),
+    );
   }
 }
